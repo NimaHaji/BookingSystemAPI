f1286cb0d5090402953198e0dd66ee693e7d4684
Nima Haji <nimahaji1395@gmail.com>
2026-05-29 16:17:45 +0330

    flow fixed login

--- Application/Features/Auth/Interfaces/IUserRepository.cs
+++ Application/Features/Auth/Interfaces/IUserRepository.cs
@@ -6,10 +6,12 @@ namespace Application.Features.Auth.Interfaces;
 public interface IUserRepository
 {
     Task RegisterUserAsync(User user);
-    Task<User?> GetUserByEmailAsync(string email);
+    Task<List<User>?> GetUsersByEmailAsync(string email);
+    Task<List<Domain.Entities.Tenant?>> GetIdentityByEmailAsync(string email);
     Task<bool> IsUserExistsByIdAsync(Guid userId);
     Task<bool> IsUserExistsByEmailAsync(string email);
     Task SaveChangesAsync();
     Task<List<ViewUser>> GetAllUsersAsync();
     Task<User?> GetUserByIdAsync(Guid userId);
+    Task<User?> GetUserByEmailAndTenantIdAsync(string email, Guid tenantId);
 }
